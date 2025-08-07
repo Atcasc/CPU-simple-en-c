52692bc750b1b38bcfd2c258edd94bcde4e2a726
Atcasc <sanchezcampelojorge@gmail.com>
2025-08-07 16:09:08 +0200

    Solucion problemas

--- CPU.c
+++ CPU.c
@@ -9,14 +9,14 @@ int main(){
     int memoria[20]={0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19};;
     char cadena[256];
     printf("Introduce la ruta del archivo:");
-    scanf("%s",cadena);
+    scanf(" %[^\n]", cadena);
+
     readingFile=fopen(cadena,"r");
     if(readingFile==NULL){
         printf("fallo en la apertura del fichero");
         return 1;
     }
-    program(readingFile,memoria);
-    return 0;
+    return  program(readingFile,memoria);
 }
 
 int program(FILE *readingFile, int memoria[]){
@@ -26,13 +26,17 @@ int program(FILE *readingFile, int memoria[]){
     while(fscanf(readingFile,"%s %d",linea,&datos)!=EOF){
         if(strcmp(linea,"LOAD")==0){
             registro=memoria[datos];
+            printf("load hecho");
         }else if (strcmp(linea,"STORE")==0){
             memoria[datos]=registro;
+            printf("store hecho");
         }else if(strcmp(linea,"ADD")==0){
             registro+=memoria[datos];
+            printf("add hecho");
         }else if(strcmp(linea,"SUB")==0){
             registro-=memoria[datos];
+            printf("sub hecho");
         }
     }
-    return 0;
+    return registro;
 }
